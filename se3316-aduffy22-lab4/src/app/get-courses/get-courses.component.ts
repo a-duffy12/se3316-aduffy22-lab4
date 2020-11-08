4889f54af4de3f9e2a3c97ae2b47b4a4b9d8e166
Aiden <12aduffy@gmail.com>
2020-11-08 18:21:42 -0500

    [Feat] Completed get-courses component
    + The get-courses component is complete in terms of structure and functionailty
    + Outputs the data received in a different manner depending on what is returned
    ~ Still need to implement the back end calls properly to use
    ~ Still need to add CSS so it looks nice

--- se3316-aduffy22-lab4/src/app/get-courses/get-courses.component.ts
+++ se3316-aduffy22-lab4/src/app/get-courses/get-courses.component.ts
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetCoursesComponent implements OnInit {
 
+  // member variables for search fields
   subject: string = "";
   catalog: string = "";
   component: string = "";
+
+  // member variables to hold output
+  data1: any;
+  data2: any;
+  data3a: any;
+  data3b: any;
+  error: string = "";
 
   constructor(private http: HttpClient) { }
 
@@ -20,26 +28,54 @@ export class GetCoursesComponent implements OnInit {
   // method to run all the various get requests for course data
   display()
   {
+    this.reset(); // reset all member variables
+
     if ((this.subject == "") && (this.catalog == "") && (this.component == "")) // Q1
     {
+      // request to back end
+      this.http.get("/api/courses").subscribe((data:any) => {
+        this.data1 = data; // get data object
+      })
       console.log("Searched all courses");
     }
     else if ((this.subject != "") && (this.catalog == "") && (this.component == "")) // Q2
     {
+      // request to back end
+      this.http.get(`/api/courses/${this.subject}`).subscribe((data:any) => {
+        this.data1 = data; // get data object
+      })
       console.log(`Search for courses with subject: ${this.subject}`);
     }
     else if ((this.subject != "") && (this.catalog != "") && (this.component == "")) // Q3a
     {
+      // request to back end
+      this.http.get(`/api/courses/${this.subject}/${this.catalog}`).subscribe((data:any) => {
+        this.data1 = data; // get data object
+      })
       console.log(`Search for courses with subject: ${this.subject} and catalog number: ${this.catalog}`);
     }
     else if ((this.subject != "") && (this.catalog != "") && (this.component != "")) // Q3b
     {
+      // request to back end
+      this.http.get(`/api/courses/${this.subject}/${this.catalog}/${this.component}`).subscribe((data:any) => {
+        this.data1 = data; // get data object
+      })
       console.log(`Search for courses with subject: ${this.subject}, catalog number: ${this.catalog}, and component: ${this.component}`);
     }
     else
     {
+      this.error = "Invalid input in search fields!";
       console.log("Invalid input!");
     }
   }
 
+  // method to reset all member varaibles used in *ngIf statements
+  reset()
+  {
+    this.data1 = undefined;
+    this.data2 = undefined;
+    this.data3a = undefined;
+    this.data3b = undefined;
+    this.error = "";
+  }
 }
